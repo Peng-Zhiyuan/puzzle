5d7c34071bcdfd7b473e6a7b67b626cb7467d0d2
zhiyuan.peng <pengzhiyuan@edroity.com>
2018-07-20 20:25:36 +0800

    大动工

--- Assets/Game.cs
+++ Assets/Game.cs
@@ -5,12 +5,25 @@ using System;
 
 public class Game : MonoBehaviour 
 {
+	void Start()
+	{
+		StaticDataLite.Init();
+		UIEngine.Init();
+		UIEngine.Forward("ShopPage");
+	}
 
-	void Start () {
+
+	void TestCore () 
+	{
 		UnityEngine.Random.InitState(DateTime.UtcNow.Second);
 		Puzzle.Instance.Init();
 		var texture = Resources.Load<Texture2D>("mapbg");
 		Puzzle.Instance.StartPuzzle(texture, 200);
+	}
+
+	void Update()
+	{
+		UpdateManager.Update();
 	}
 
 }
